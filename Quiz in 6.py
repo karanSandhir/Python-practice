42030096469cf6a23aa96732bdf4e490cc71b593
Karan Sandhir <Karansandhir03@gmail.com>
2025-09-04 05:19:29 +0530

    Add files via upload

---
+++ Quiz in 6.py
@@ -0,0 +1,5 @@
+a=int (input("enter ur age = "))
+if(a>=18):
+    print("yes")
+else:
+    print("no")
---
+++ Quiz in 8.py
@@ -0,0 +1,10 @@
+#def quick():
+ #   print("have a good day")
+#quick()
+def quiz(a,b):
+    print("wlcome to the quiz"+a)
+    print(f"yr age is 10")
+    return "ok"
+a=quiz("sachin",10)
+print(a)
+          
